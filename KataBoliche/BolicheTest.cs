c00fe53b2ec2fd08f2e1ee9d115ef078e70f3d53
Davy Monteoliva <dmonteoliva@gmail.com>
2016-06-21 01:35:05 -0300

    Resolução do Kata do boliche do Uncle Bob

---
+++ KataBoliche/BolicheTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataBoliche
+{
+    [TestClass]
+    public class BolicheTest
+    {
+        private Jogo j;
+
+        private void FazerMuitasJogadas(int jogadas, int pinosDerrubados)
+        {
+            for (int i = 0; i < jogadas; i++)
+                j.FazerJogada(pinosDerrubados);
+        }
+
+        [TestInitialize]
+        public void IniciarJogo()
+        {
+            j = new Jogo();
+        }
+
+        [TestMethod]
+        public void JogoVazio()
+        {
+            j.FazerJogada(0);
+            Assert.AreEqual(0, j.Placar());
+        }
+
+        [TestMethod]
+        public void AcertandoSempreUm()
+        {
+            FazerMuitasJogadas(20, 1);
+            Assert.AreEqual(20, j.Placar());
+        }
+
+        [TestMethod]
+        public void UmSpare()
+        {
+            j.FazerJogada(3);
+            j.FazerJogada(7);
+            j.FazerJogada(4);
+            j.FazerJogada(2);
+
+            Assert.AreEqual(20, j.Placar());
+        }
+
+        [TestMethod]
+        public void UmStrike()
+        {
+            j.FazerJogada(10);
+            j.FazerJogada(3);
+            j.FazerJogada(6);
+
+            Assert.AreEqual(28, j.Placar());
+        }
+
+        [TestMethod]
+        public void JogoPerfeito()
+        {
+            FazerMuitasJogadas(20, 10);
+            Assert.AreEqual(300, j.Placar());
+        }
+    }
+}
---
+++ KataBoliche/Jogo.cs
@@ -0,0 +1,66 @@
+﻿namespace KataBoliche
+{
+    public class Jogo
+    {
+        private int[] Jogadas = new int[21];
+        private int JogadaAtual = 0;
+
+        public void FazerJogada(int pinosDerrubados)
+        {
+            Jogadas[JogadaAtual] = pinosDerrubados;
+            JogadaAtual++;
+        }
+
+        public int Placar()
+        {
+            int placarAtual = 0;
+            int bolaAtual = 0;
+
+            for (int quadro = 0; quadro < 10; quadro++)
+            {
+                if (Strike(bolaAtual))
+                {
+                    placarAtual += ProximasDuasBolasParaStrike(bolaAtual);
+                    bolaAtual++;
+                }
+                else if (Spare(bolaAtual))
+                {
+                    placarAtual += 10 + ProximaBolaParaSpare(bolaAtual);
+                    bolaAtual += 2;
+                }
+                else
+                {
+                    placarAtual += DuasBolasParaQuadroAtual(bolaAtual);
+                    bolaAtual += 2;
+                }
+            }
+
+            return placarAtual;
+        }
+
+        private bool Strike(int i)
+        {
+            return Jogadas[i] == 10;
+        }
+
+        private int ProximasDuasBolasParaStrike(int bolaAtual)
+        {
+            return 10 + Jogadas[bolaAtual + 1] + Jogadas[bolaAtual + 2];
+        }
+
+        private bool Spare(int i)
+        {
+            return Jogadas[i] + Jogadas[i + 1] == 10;
+        }
+
+        private int ProximaBolaParaSpare(int bolaAtual)
+        {
+            return Jogadas[bolaAtual + 2];
+        }
+
+        private int DuasBolasParaQuadroAtual(int bolaAtual)
+        {
+            return Jogadas[bolaAtual] + Jogadas[bolaAtual + 1];
+        }
+    }
+}
